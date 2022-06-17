393cbc189b50136adf8d725b4dca4450b7df40bd
Alabhya Jindal <alabhya10@gmail.com>
2022-06-18 00:04:15 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import ReactFlow, { ReactFlowProvider } from 'react-flow-renderer';
 
-function App() {
+const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+  { id: '2', data: { label: 'Default Node' }, position: { x: 250, y: 125 } },
+];
+
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  function createNode() {
+    setNodes((n) => {
+      return [
+        ...n,
+        {
+          id: '3',
+          data: { label: 'Third Node' },
+          position: { x: 250, y: 225 },
+        },
+      ];
+    });
+  }
+
+  function createEdge() {
+    setEdges((e) => {
+      return [...e, { id: 'e2-3', source: '2', target: '3' }];
+    });
+  }
+
+  window.addEventListener('keydown', (e) => {
+    handleKeyDown(e.key);
+  });
+
+  function handleKeyDown(key) {
+    if (key === 's') {
+      createNode();
+      createEdge();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='app'>
+      <h1>Welcome to Fast Flow</h1>
+      <ReactFlowProvider>
+        <ReactFlow id='flow' nodes={nodes} edges={edges} fitView />
+      </ReactFlowProvider>
     </div>
   );
 }
-
-export default App;
